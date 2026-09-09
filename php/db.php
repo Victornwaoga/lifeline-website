6aaa77a8d48ed5495e34b5b6c84e7fdae426d2aa
Victornwaoga <ogbonnayamathias702@gmail.com>
2026-09-09 14:21:28 -0700

    Changed password

--- php/db.php
+++ php/db.php
@@ -20,7 +20,7 @@ if (getenv("DATABASE_URL")) {
     $port = "5432";
     $dbname = "lifeline";
     $user = "postgres";
-    $password = "password";
+    $password = "~!qw@#$%";
 }
 
 $conn = pg_connect(
