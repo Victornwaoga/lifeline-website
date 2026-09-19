327314d5a481db92c327ebaab1166c0f3d19c87c
Victornwaoga <ogbonnayamathias702@gmail.com>
2026-09-19 06:04:44 -0700

    A third commmit today

--- request-details.php
+++ request-details.php
@@ -51,6 +51,7 @@ $request = pg_fetch_assoc($result);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Request Details | LifeLine</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/request-details.css">
 </head>
 
 <body>
